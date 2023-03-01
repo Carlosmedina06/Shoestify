7bb0c7669667a262e4e88673ba7ac5d417e39877
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-01 12:25:50 -0300

    modelo user

---
+++ server/src/models/user.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  allyourPurchase: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+})
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
