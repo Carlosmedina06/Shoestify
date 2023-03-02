3f5eb3589fdc80f0c70779e0e6d9d549d07a2f21
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-02 00:53:18 -0300

    agregue context?

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import { createServer } from '../server/src/app.js'
 
 import { typeDefs } from './src/graphql/typedef/typedefs.js'
+import { context } from './src/graphql/context/context.js'
 import { resolvers } from './src/graphql/resolvers/resolvers.js'
 import { connect } from './src/db.js'
 
-createServer(typeDefs, resolvers)
+createServer(typeDefs, resolvers, context)
 connect()
