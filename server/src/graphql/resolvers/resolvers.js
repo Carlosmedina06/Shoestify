489efa9a84f9f6509b454865f2809ec4f68b14ef
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-03 13:24:15 -0300

    resolvers product

--- server/src/graphql/resolvers/resolvers.js
+++ server/src/graphql/resolvers/resolvers.js
@@ -1,14 +1,26 @@
 import { createUser, getUserById, getUsers, deleteUser, loginUser } from './user.js'
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from './product.js'
 
 export const resolvers = {
   Query: {
     users: getUsers,
     userById: getUserById,
+    products: getProducts,
+    productById: getProductById,
   },
 
   Mutation: {
     createUser,
     deleteUser,
     loginUser,
+    createProduct,
+    updateProduct,
+    deleteProduct,
   },
 }
