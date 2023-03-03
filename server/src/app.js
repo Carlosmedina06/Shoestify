c2c0b81b46baa8a4523f5d5efc98b3aab46a7d28
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-03 13:22:37 -0300

    cambio config server para pasar context

--- server/src/app.js
+++ server/src/app.js
@@ -5,21 +5,25 @@ import cors from 'cors'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 
-export async function createServer(typeDefs, resolvers, context) {
+export async function createServer(typeDefs, resolvers) {
   const app = express()
-  const serverHttp = http.createServer(app)
+  const httpServer = http.createServer(app)
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context,
   })
 
   await server.start()
+  app.use(
+    '/graphql',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.authorization }),
+    }),
+  )
 
-  app.use('/graphql', cors(), express.json(), expressMiddleware(server))
-
-  await new Promise((resolve) => serverHttp.listen({ port: 4000 }, resolve))
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
   // eslint-disable-next-line no-console
-
-  console.log(`🚀 Server ready at http://localhost:4000`)
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`)
 }
