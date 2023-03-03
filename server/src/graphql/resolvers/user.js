6452b395e03af8f4b31dcc3196ac67507dde4206
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-03 13:24:27 -0300

    logica actualizada user

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -3,21 +3,31 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 
 import User from '../../models/user.js'
+import { checkAuthorization } from '../utils/checkAuthorization.js'
+
 dotenv.config()
 
 const SALT_ROUNDS = 10
 
-export const getUsers = async (parent, args, context) => {
-  if (!context.loggedInUser) throw new Error('You are not authorized to do this action')
+export const getUsers = async (_parent, _args, context) => {
+  try {
+    checkAuthorization(context)
 
-  return await User.find({})
+    return await User.find({})
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
-export const getUserById = async (parent, { id }, context) => {
-  if (!context.loggedInUser) throw new Error('You are not authorized to do this action')
-  const user = await User.findById(id)
+export const getUserById = async (_parent, { id }, context) => {
+  try {
+    checkAuthorization(context)
+    const user = await User.findById(id)
 
-  return user
+    return user
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 export const createUser = async (_, { name, email, password }) => {
@@ -32,10 +42,20 @@ export const createUser = async (_, { name, email, password }) => {
       password: await bcrypt.hash(password, SALT_ROUNDS),
     })
 
-    return {
+    const userForToken = {
       id: user._id,
       email: user.email,
-      token: jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d' }),
+      isAdmin: user.isAdmin,
+      active: user.active,
+      allyourPurchase: user.allyourPurchase,
+      name: user.name,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1d' })
+
+    return {
+      id: user._id,
+      token,
     }
   } catch (error) {
     throw new Error(error.message)
@@ -44,9 +64,9 @@ export const createUser = async (_, { name, email, password }) => {
 
 export const deleteUser = async (_, { id }, context) => {
   try {
-    if (!context.user) throw new Error('You are not authorized to do this action')
     if (!id) throw new Error('Please provide an id')
-    if (context.user.isAdmin === false) throw new Error('You are not authorized to do this action')
+    checkAuthorization(context)
+
     const user = await User.findById(id)
 
     if (!user) throw new Error('User does not exist')
@@ -70,10 +90,20 @@ export const loginUser = async (_, { email, password }) => {
 
     if (!isMatch) throw new Error('Invalid credentials')
 
+    const userForToken = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      active: user.active,
+      allyourPurchase: user.allyourPurchase,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1d' })
+
     return {
       id: user._id,
-      email: user.email,
-      token: jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d' }),
+      token,
     }
   } catch (error) {
     throw new Error(error.message)
