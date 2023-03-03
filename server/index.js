63f0cf6e044933cefaf5cee05c0a1dd7cfe8e9c1
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-03 13:22:57 -0300

    cambio index config para context

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 import { createServer } from '../server/src/app.js'
 
 import { typeDefs } from './src/graphql/typedef/typedefs.js'
-import { context } from './src/graphql/context/context.js'
 import { resolvers } from './src/graphql/resolvers/resolvers.js'
 import { connect } from './src/db.js'
 
-createServer(typeDefs, resolvers, context)
+createServer(typeDefs, resolvers)
 connect()
