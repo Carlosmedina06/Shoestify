09c4de315639c2496a45ad5407cba99b98779f27
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-03 13:23:43 -0300

    chequear autorizacion

---
+++ server/src/graphql/utils/checkAuthorization.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export function checkAuthorization(context) {
+  if (!context.token) throw new Error('You are not authorized to do this action')
+
+  const authorization = context.token
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.substring(7)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (decodedToken.isAdmin === false) {
+      throw new Error('You are not authorized to do this action')
+    }
+  }
+}
