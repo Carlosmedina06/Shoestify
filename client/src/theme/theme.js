f84b96eec284a9eaff206e7902b6d6a14c3f36b6
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-06 16:16:00 -0300

    cambio en el tema

--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -1,11 +1,46 @@
 import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
 const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
   colors: {
     brand: {
       primario: '#000000',
       secundario: '#ffffff',
       terciario: '#484848',
+    },
+    styles: {
+      global: (props) => ({
+        'html, body, #root': {
+          color: mode(undefined, 'whiteAlpha.900')(props),
+          height: '100%',
+        },
+        '*::-webkit-scrollbar': {
+          display: 'none',
+        },
+      }),
+    },
+    textStyles: {
+      translucid: {
+        '.chakra-ui-light &': {
+          color: ['blackAlpha.100', 'blackAlpha.500'],
+        },
+        '.chakra-ui-dark &': {
+          color: ['whiteAlpha.100', 'whiteAlpha.500'],
+        },
+      },
+    },
+    fontSizes: {
+      sm: '0.95rem',
+      xs: '0.9rem',
+    },
+    sizes: {
+      container: {
+        xl: '1265px',
+      },
     },
   },
 })
