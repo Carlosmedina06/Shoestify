c41a87a835ac26d6ccaa43f55be68fb50a210de9
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-06 16:15:18 -0300

    borrar card de prueba

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { Card, CardBody, Text } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 export const Home = () => {
-  return (
-    <Card>
-      <CardBody>
-        <Text>View a summary of all your customers over the last month.</Text>
-      </CardBody>
-    </Card>
-  )
+  return <Text color={'whatsapp.100'}>HOLA</Text>
 }
