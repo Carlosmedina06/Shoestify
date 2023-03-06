e251cfeec48cdf0982e4a56ecc4e6c28c86ac25d
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-06 16:15:09 -0300

    mejora de layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,15 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { Container, useColorModeValue } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
 import Navbar from '../components/Navbar/Navbar'
 
 export const Layout = () => {
+  const dark = useColorModeValue('brand.primario', 'brand.secundario')
+
   return (
-    <Box bg="brand.primario" h="100vh" w="100">
-      <main>
-        <Navbar />
-        <Outlet />
-      </main>
-    </Box>
+    <Container alignSelf="center" bg={dark} height="100%" maxWidth="container" paddingX={0}>
+      <Navbar />
+      <Outlet />
+    </Container>
   )
 }
