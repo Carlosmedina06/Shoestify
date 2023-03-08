44574e9763b6b1ddb2629e041816064028a6882c
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-08 17:51:33 -0300

    cambio margin home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,25 +3,30 @@ import { Center, Image, Stack, Text, useColorModeValue } from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
 
 import Banner from '../../components/Banner/Banner'
-import CarouselApp from '../../components/Carousel/Carousel'
+import Carousel from '../../components/Carousel/Carousel'
 export const Home = () => {
   const dark = useColorModeValue('brand.primario', 'brand.secundario')
   const light = useColorModeValue('brand.secundario', 'brand.primario')
 
   return (
     <>
-      <Center px={{ base: '4', md: '60' }} py={20}>
-        <Stack alignItems={'center'} color={light} direction={{ base: 'column', md: 'row' }}>
+      <Center my={20} px={{ base: '4', md: '60' }} py={20}>
+        <Stack
+          alignItems={'center'}
+          color={light}
+          direction={{ base: 'column', md: 'row' }}
+          ml={{ base: 0, md: 7 }}
+        >
           <Stack
             alignItems={{ base: 'flex-start', md: 'center' }}
             order={{ base: 2, md: 1 }}
             spacing={'4'}
             w={{ base: '100%', md: '50%' }}
           >
-            <Text fontSize="3xl" fontWeight={'bold'} px={{ base: '4', md: '30' }}>
-              Camina con pasión y estilo Descubre nuestra tienda en línea de zapatos
+            <Text fontSize="4xl" fontWeight={'bold'}>
+              Camina con pasión y estilo. Descubre nuestra tienda en línea de zapatos
             </Text>
-            <Text fontSize="md" px={{ base: '4', md: '30' }}>
+            <Text fontSize="xl">
               Encuentra el estilo perfecto con zapatos duraderos y cómodos. Explora nuestra
               colección ahora y eleva tu moda.
             </Text>
@@ -42,7 +47,6 @@ export const Home = () => {
                   color={dark}
                   cursor={'pointer'}
                   justifyItems={'center'}
-                  ml={{ base: 0, md: 7 }}
                   px={4}
                   py={1}
                   rounded={'md'}
@@ -65,7 +69,7 @@ export const Home = () => {
           >
             <Image
               alt="Segun Adebayo"
-              boxSize="300px"
+              boxSize="400px"
               justifySelf={'center'}
               objectFit="cover"
               src="https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
@@ -87,13 +91,13 @@ export const Home = () => {
           <Text fontSize="3xl" fontWeight={'bold'} px={{ base: '4', md: '30' }}>
             Nuestras marcas
           </Text>
-          <Text fontSize="md" px={{ base: '4', md: '30' }}>
+          <Text fontSize="xl" px={{ base: '4', md: '30' }}>
             Encuentra el estilo perfecto con zapatos duraderos y cómodos. Explora nuestra colección
             ahora y eleva tu moda.
           </Text>
         </Stack>
       </Center>
-      <CarouselApp />
+      <Carousel />
     </>
   )
 }
