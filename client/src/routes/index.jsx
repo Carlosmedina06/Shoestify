0e0dcfb803a61dee83ca7977f6e8839f8dc459a9
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-22 14:44:53 -0300

    nuevas rutas

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -4,6 +4,9 @@ import CreateProduct from '../components/CreateProduct/CreateProduct'
 import { Layout } from '../layout/Layout'
 import { Home } from '../pages/home/Home'
 import Products from '../pages/Products/Products'
+import ProductsHombre from '../pages/Products/ProductsHombre'
+import ProductsMujer from '../pages/Products/ProductsMujer'
+import ProductsNinos from '../pages/Products/ProductsNinos'
 
 export const router = createBrowserRouter([
   {
@@ -20,15 +23,15 @@ export const router = createBrowserRouter([
       },
       {
         path: '/products/hombres',
-        element: <Products />,
+        element: <ProductsHombre />,
       },
       {
         path: '/products/Mujeres',
-        element: <Products />,
+        element: <ProductsMujer />,
       },
       {
         path: '/products/Niños',
-        element: <Products />,
+        element: <ProductsNinos />,
       },
       {
         path: '/createProduct',
