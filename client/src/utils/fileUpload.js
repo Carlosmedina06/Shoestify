1d91b6c7e7c1f40dd6152598aa562133aa855e8f
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-24 17:10:01 -0300

    variables de entorno actualizadas

--- client/src/utils/fileUpload.js
+++ client/src/utils/fileUpload.js
@@ -1,9 +1,9 @@
 export const fileUpload = async (file) => {
-  const cloudinaryUrl = `process.env.REACT_APP_CLOUDINARY_URL`
+  const cloudinaryUrl = `${import.meta.env.VITE_REACT_APP_CLOUDINARY_URL}`
 
   const formData = new FormData()
 
-  formData.append('upload_preset', `${process.env.REACT_APP_CLOUDINARY_PRESET}`)
+  formData.append('upload_preset', `${import.meta.env.VITE_REACT_APP_CLOUDINARY_UPLOAD_PRESET}`)
   formData.append('file', file)
 
   try {
