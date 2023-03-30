2e45506722de3827dec7e11a8708a4f478eaa9ef
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-30 18:20:21 -0300

    fix error en el layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,14 +1,20 @@
-import { Container, useColorModeValue } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
 import Footer from '../components/Footer/Footer'
 import Navbar from '../components/Navbar/Navbar'
 
 export const Layout = () => {
-  const dark = useColorModeValue('brand.primario', 'brand.secundario')
-
   return (
-    <Container alignSelf="center" bg={dark} height="100%" maxWidth="container" paddingX={0}>
+    <Container
+      display="flex"
+      flexDirection="column"
+      gap={16}
+      h="100%"
+      justifyContent="space-between"
+      maxWidth="container"
+      paddingX={0}
+    >
       <Navbar />
       <Outlet />
       <Footer />
