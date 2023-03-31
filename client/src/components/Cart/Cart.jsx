b4b8e925c846c1161df1de39bb6159ffa0a3b714
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-30 23:31:36 -0300

    handleRemove

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   Button,
   Divider,
   Drawer,
@@ -17,14 +18,20 @@ import { Icon } from '@chakra-ui/icons'
 import { IoCartOutline } from 'react-icons/io5'
 
 import useProductStore from '../../store/productStore'
+import CartCard from '../CartCard/CartCard'
 
 function Cart() {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const dark = useColorModeValue('brand.primario', 'brand.secundario')
   const light = useColorModeValue('brand.secundario', 'brand.primario')
 
+  const { removeCart } = useProductStore()
   const cart = useProductStore((state) => state.cart)
   const totalPrice = cart.reduce((acc, product) => acc + product.price * product.quantity, 0)
+
+  const handleRemove = (id) => {
+    removeCart(id)
+  }
 
   return (
     <>
@@ -36,50 +43,43 @@ function Cart() {
         }}
         bg={dark}
         color={light}
+        position={'relative'}
         onClick={onOpen}
       >
         <Icon as={IoCartOutline} boxSize={6} />
+        {cart.length > 0 && (
+          <Stack position={'absolute'} right={'2'} top={'0'}>
+            <Badge bg={'red'} borderRadius={'full'} color={light}>
+              {cart.length}
+            </Badge>
+          </Stack>
+        )}
       </Button>
       <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
         <DrawerOverlay />
         <DrawerContent bg={dark} color={light}>
           <DrawerCloseButton color={light} />
-          <DrawerHeader>Shopping Cart 🛒</DrawerHeader>
+          <DrawerHeader>Carrito de compra 🛒</DrawerHeader>
           <DrawerBody h={'100%'}>
-            <Stack h={'100%'} justifyContent={'space-between'} spacing={4}>
-              <Stack>
+            <Stack h={'100%'} justifyContent={'space-between'}>
+              <Stack p={2} spacing={2}>
                 {cart.map((product) => (
-                  <Stack key={product.id} direction={'row'} spacing={4}>
-                    <Text>{product.name}</Text>
-                    <Text>${product.price}</Text>
-                    <Text>cantidad: {product.quantity}</Text>
-                  </Stack>
+                  <CartCard key={product.id} handleRemove={handleRemove} product={product} />
                 ))}
               </Stack>
-              <Stack>
-                <Text>Total: ${totalPrice}</Text>
-                <Divider />
-              </Stack>
             </Stack>
+            <Divider />
           </DrawerBody>
-          <DrawerFooter>
-            <Button
-              _hover={{
-                bg: light,
-                color: dark,
-              }}
-              mr={3}
-              variant="outline"
-              onClick={onClose}
-            >
-              Cancel
-            </Button>
+          <DrawerFooter display={'flex'} gap={4} justifyContent={'space-between'} w={'100%'}>
+            <Text fontWeight={'bold'}>Total: ${totalPrice}</Text>
+
             <Button
               _active={{
                 transform: 'scale(0.95)',
               }}
               bg={light}
               color={dark}
+              width={'60%'}
             >
               Buy
             </Button>
