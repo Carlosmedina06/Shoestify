d26419db838889bed0111fbd03160b91679a7cf9
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-02 17:06:27 -0300

    pruebas en el profile

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -2,11 +2,9 @@ import {
   Avatar,
   Box,
   Card,
-  Center,
   Flex,
   Heading,
   Image,
-  SimpleGrid,
   Stack,
   Text,
   useColorModeValue,
@@ -21,12 +19,11 @@ const Profile = () => {
   const user = useAuthStore((state) => state.user)
   const productIds = user?.allyourPurchase?.map((product) => product)
 
-  console.log(user)
+  const { data, loading } = useProductsUser(productIds)
 
-  const { data, loading, error } = useProductsUser(productIds)
+  if (loading) return <p>Loading...</p>
 
   console.log(data)
-  if (loading) return <p>Loading...</p>
 
   return (
     <Stack
@@ -34,11 +31,18 @@ const Profile = () => {
         base: 'center',
         md: 'flex-start',
       }}
+      border={'2px'}
+      borderRadius={'lg'}
       direction={{
         base: 'column',
         md: 'row',
       }}
-      px={4}
+      m={{
+        base: 0,
+        md: '32',
+      }}
+      px={8}
+      py={8}
     >
       <Flex
         alignItems={'center'}
@@ -62,16 +66,43 @@ const Profile = () => {
         </Heading>
         <Text>{user?.email}</Text>
       </Flex>
-      <Box border={'1px'} w={'100%'}>
-        <Heading as={'h3'} size={'md'}>
+      <Box w={'100%'}>
+        <Heading
+          as={'h3'}
+          fontWeight={'medium'}
+          letterSpacing={'2.5px'}
+          pl={{
+            base: 0,
+            md: 6,
+          }}
+          py={4}
+          size={'md'}
+          textTransform={'uppercase'}
+        >
           Tus compras
         </Heading>
-        <Stack>
+        <Stack
+          pl={{
+            base: 0,
+            md: 6,
+          }}
+          spacing={6}
+          w={{
+            base: '100%',
+            md: '80%',
+          }}
+        >
           {data?.productsByArray?.map((product) => (
-            <Card key={product.id}>
-              <Image src={product.image} />
-              <Text>{product.name}</Text>
-              <Text>{product.price}</Text>
+            <Card key={product.id} p={4}>
+              <Stack alignItems={'center'} direction={'row'} spacing={6}>
+                <Image height={16} objectFit={'cover'} rounded={4} src={product.image} width={16} />
+                <Stack>
+                  <Text fontWeight={600}>{product.name}</Text>
+                  <Text color={'gray.500'} fontSize={'sm'}>
+                    {product.brand}
+                  </Text>
+                </Stack>
+              </Stack>
             </Card>
           ))}
         </Stack>
