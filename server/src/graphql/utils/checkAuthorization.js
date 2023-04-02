e2a3878f56d5d14ecb44bf11ee7a98083365c292
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-01 22:28:20 -0300

    fix logica checkUser

--- server/src/graphql/utils/checkAuthorization.js
+++ server/src/graphql/utils/checkAuthorization.js
@@ -25,8 +25,7 @@ export function checkUser(context) {
 
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     const token = authorization.substring(7)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-    return decodedToken
+    return token
   }
 }
