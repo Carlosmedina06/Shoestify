ed28a9b8dce65089bc989976f9b4957fddab8580
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-02 17:03:47 -0300

    cambio imagenes del carousel por los productos reales

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,16 +1,12 @@
 import { Image, Stack } from '@chakra-ui/react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+
 import 'swiper/css'
+import useProductStore from '../../store/productStore'
 
 const Carousel = () => {
-  const images = [
-    'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=796&q=80',
-    'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
-    'https://images.unsplash.com/photo-1615028456268-02eb9815a776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80',
-  ]
-
+  const productos = useProductStore((state) => state.products)
+  const images = productos.map((product) => product.image)
   const shuffledImages = images.sort(() => Math.random() - 0.5)
 
   return (
